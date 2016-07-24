c04daf6bc477ea9c26e2318b62397cbd8fbf345f
upperstream <developer@upperstream.jp>
2016-07-24 11:27:20 +0900

    Add `--files` and `--encoding` options.
    These options give a list of files to test, instead of specifying MP3 file
    names on the command line.

--- README.md
+++ README.md
@@ -7,18 +7,27 @@ Checks is a proper ID3 tag is added to the specified MP3 file.
 ## Motivation
 
 My SatNav/Player cannot read MP3 ID3 tag version 2.x but only version 1.0,
-meanwhile some MP3 files come with ID3v2.x tag only.  This makes me write
+meanwhile some MP3 files come without ID3v1 tag.  This makes me write
 a tool to test and report if an MP3 file has an ID3v1 tag.
 
 ## Usage
 
     id3stat mp3file [...]
+    id3stat --files=<list> --encoding=<encoding>
     id3stat -L
     id3stat -V
     id3stat -H
 
 The first syntax checks if a specified MP3 file has an ID3v1 tag and prints
-the file name if the file has no ID3v1 tag.  Two or more files can be specified.
+the file name if the file has no ID3v1 tag.  Two or more files can be
+specified.
+
+The second syntax gives a list of files to test, with specifying the encoding
+of the content of the list file.  The _list_ parameter specifies the file
+name of a text file consisting lines that have an MP3 file name on each.
+The _encoding_ parameter is the encoding of the list file itself, neither
+the encoding of MP3 files nor of MP3 file names on the file system.
+Currently `UTF-8` (default) and `ShiftJIS` are supported.
 
 The `-L` flag indicates to display a licensing notice.  The `-V` flag
 indicates to display the version number of `id3stat`.  The `-H` flag
@@ -26,7 +35,7 @@ indicates to display the usage help.
 
 ## Limitation
 
-By the original requirement, `id3stat` only supports ID3v1 tag.
+By the original requirement, `id3stat` supports ID3v1 tag only.
 
 ## Licensing
 
@@ -34,3 +43,16 @@ This tool is licensed under the
 [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
 Different licences may apply to depending packages.  See `NOTICE.txt`
 file for details.
+
+## How to build
+
+    cd $(GOPATH)/src
+    go get -u github.com/dhowden/tag \
+              golang.org/x/text/encoding \
+              golang.org/x/text/encoding/japanese \
+              golang.org/x/text/transform
+    go build github.com/upperstream/id3stat
+
+When you modify `NOTICE.txt`, you have to execute `go generate` so that `notice.go` gets updated:
+
+    go generate github.com/upperstream/id3stat
--- id3stat.go
+++ id3stat.go
@@ -17,11 +17,17 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 const appVersion = "0.0.1"
@@ -29,6 +35,9 @@ const appVersion = "0.0.1"
 var versionFlag = flag.Bool("V", false, "Print the version number.")
 var licenceFlag = flag.Bool("L", false, "Print the licencing notice.")
 var helpFlag = flag.Bool("H", false, "Print this message.")
+var filesFlag = flag.String("files", "", "Provides a list of files to process.")
+var encodingFlag = flag.String("encoding", "UTF-8",
+	"Encoding of a file that -files flag provides.")
 
 type id3Error struct {
 	Path string
@@ -57,17 +66,55 @@ func main() {
 		os.Exit(2)
 		return
 	}
-	if flag.NArg() == 0 {
-		flag.PrintDefaults()
-		return
+	if err := validateEncodingFlag(*encodingFlag); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 
-	var nError int
-	_, nError = getFileStatuses(os.Args[len(os.Args)-flag.NArg() : len(os.Args)])
-	if nError == 0 {
-		os.Exit(0)
+	var files []string
+	var err error
+	if len(*filesFlag) > 0 {
+		if flag.NArg() > 0 {
+			fmt.Fprintf(os.Stderr,
+				"You cannot specify command line arguments when you provide --files option.\n\n")
+			printUsage()
+			os.Exit(2)
+		}
+		files, err = parseListFile(*filesFlag, *encodingFlag)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 	} else {
+		if flag.NArg() == 0 {
+			printUsage()
+			os.Exit(2)
+		}
+		files = os.Args[len(os.Args)-flag.NArg() : len(os.Args)]
+	}
+	nSuccess, _ := getFileStatuses(files)
+	if nSuccess == 0 {
 		os.Exit(1)
+	} else {
+		os.Exit(0)
+	}
+}
+
+func printUsage() {
+	fmt.Println(os.Args[0], "mp3file [...]")
+	fmt.Println(os.Args[0], "--files=<list> --encoding=<encoding>")
+	fmt.Println(os.Args[0], "-H | -L | -V")
+	flag.PrintDefaults()
+}
+
+func validateEncodingFlag(encoding string) error {
+	switch encoding {
+	case "ShiftJIS":
+		return nil
+	case "", "UTF-8":
+		return nil
+	default:
+		return fmt.Errorf("Unsupported encoding: %s", encoding)
 	}
 }
 
@@ -102,6 +149,38 @@ func getFileStatus(pathname string) error {
 		}
 	}
 	return nil
+}
+
+func newReader(reader io.Reader, encoding string) (io.Reader, error) {
+	switch encoding {
+	case "ShiftJIS":
+		return transform.NewReader(reader, japanese.ShiftJIS.NewDecoder()), nil
+	case "", "UTF-8":
+		return reader, nil
+	default:
+		return nil, fmt.Errorf("Unsupported encoding: %s", encoding)
+	}
+}
+
+func parseListFile(listfile string, encoding string) (files []string, err error) {
+	f, _ := os.Open(listfile)
+	defer f.Close()
+	var reader io.Reader
+	reader, err = newReader(f, encoding)
+	if err != nil {
+		return nil, err
+	}
+	files = make([]string, 0, 256)
+	s := bufio.NewScanner(reader)
+	for s.Scan() {
+		quoted := s.Text()
+		if filename, err1 := strconv.Unquote(quoted); err1 == nil {
+			files = append(files, filename)
+		} else {
+			files = append(files, quoted)
+		}
+	}
+	return files, nil
 }
 
 func printLicence() {
